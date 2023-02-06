8b2e9b6c84a824a8a624d79b12491a705591a2dd
🎨 <lelouch@universe.local>
2023-02-05 19:14:20 -0500

    try form submission

---
+++ help/index.js
@@ -0,0 +1,27 @@
+
+let postMsg = () => {
+    const email = DOMPurify.sanitize(document.getElementById('email').value)
+    const request = DOMPurify.sanitize(document.getElementById('help-needed').value)
+
+    let message = `{
+        "email": ${email},
+        "request": ${request},
+    }`
+
+fetch("https://vcudbl.deta.dev/users", {
+  method: "post",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+
+  //make sure to serialize your JSON body
+  body: JSON.stringify(message)
+})
+.then( (response) => { 
+   console.log("done!")
+});
+
+}
+
+document.getElementById('submit').onclick = postMsg
